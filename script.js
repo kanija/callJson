8d4049819fe7485e282faad3a9c25b5d3988707f
Danny Vernovsky <dvernovsky@interwise.com>
2019-04-13 21:53:57 +0300

    added homework for your convenience

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function getInitialData(){
+    
     // TODO: RONEN: Add your code here, it's your job!!!!!!!!!!!!!!!!11111111
+    // please use this endpoint for the first api call https://esi.evetech.net/dev/search/?categories=character&search={search_string}
+    // for the second call take the first id from array and use this one: https://esi.evetech.net/dev/characters/{character_id}/
+    // use es6 fetch api for this
+    // for example use this fetch('https://esi.evetech.net/dev/characters/93939512/').then(function(response){response.json().then(function(jsondata){console.log(jsondata)}) });
+    
+
 }
